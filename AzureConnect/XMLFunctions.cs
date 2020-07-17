a9e95639fefad06992a17a6539ebeed15814fbd2
Daniel Ramos de Oliveira <drojf@hotmail.com>
2020-07-17 19:50:06 -0300

    created Config Grupo and Server

---
+++ AzureConnect/XMLFunctions.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace AzureConnect
+{
+	public class XMLFunctions
+	{
+		#region CONFIG CREATE | LOAD | CHANGE
+
+		// CHECK IF EXIST CONFIG
+		//==============================================================================================
+		public static bool VerificaConfigXML()
+		{
+			string FindXML = Application.StartupPath + "\\Config.xml";
+
+			if (File.Exists(FindXML))
+				return true;
+			else
+				return false;
+		}
+
+		// CREATE CONFIG XML
+		//==============================================================================================
+		public static void CriarConfigXML()
+		{
+			try
+			{
+				new XDocument(
+					new XElement("Configuracao",
+						new XElement("DefaultValues",
+							new XElement("Grupo", ""),
+							new XElement("Server", "")
+						),
+						new XElement("Contas",
+							new XElement("Conta1", "conta1"),
+							new XElement("Conta2", "conta2"),
+							new XElement("Conta3", "conta3")
+						)
+					)
+				)
+				.Save("Config.xml");
+
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Exceção XML", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+		}
+
+		// GET CONFIG XML DEFAULT VALUE
+		// =============================================================================
+		public static string ObterDefault(string CampoDefault)
+		{
+			try
+			{
+				XmlDocument config = MyConfig();
+				if (config != null)
+				{
+					return config.SelectSingleNode("Configuracao").SelectSingleNode("DefaultValues").SelectSingleNode(CampoDefault).InnerText;
+				}
+				else
+				{
+					return string.Empty;
+				}
+			}
+			catch
+			{
+				return string.Empty;
+			}
+		}
+
+		// GET CONFIG XML DEFAULT VALUE
+		// =============================================================================
+		public static Dictionary<string, string> ObterContas()
+		{
+			try
+			{
+				XmlDocument config = MyConfig();
+				if (config != null)
+				{
+					Dictionary<string, string> dicContas = new Dictionary<string, string>();
+
+					var contas = config.SelectSingleNode("Configuracao").SelectSingleNode("Contas").ChildNodes;
+
+					foreach (XmlNode ct in contas)
+					{
+						dicContas.Add(ct.Name, ct.InnerText);
+					}
+
+					return dicContas;
+				}
+				else
+				{
+					return null;
+				}
+			}
+			catch
+			{
+				return null;
+			}
+		}
+
+		// GET CONFIG XML FILE
+		// =============================================================================
+		public static XmlDocument MyConfig()
+		{
+			if (VerificaConfigXML())
+			{
+				XmlDocument myXML = new XmlDocument();
+				try
+				{
+					myXML.Load(Application.StartupPath + "\\Config.xml");
+					return myXML;
+				}
+				catch (Exception ex)
+				{
+					throw ex;
+				}
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		// OBTER VALOR DO NODE XML DO ARQUIVO CONFIGXML PELO NOME
+		// =============================================================================
+		public static string ObterConfigValorNode(string NodeName)
+		{
+			XmlNodeList elemList = MyConfig().GetElementsByTagName(NodeName);
+			string myValor = "";
+
+			for (int i = 0; i < elemList.Count; i++)
+			{
+				myValor = elemList[i].InnerXml;
+			}
+
+			return myValor;
+
+		}
+
+		// SAVE CONFIG XML DEFAULT VALUE
+		// =============================================================================
+		public static bool SaveDefault(string CampoDefault, string NewValorDefault)
+		{
+			try
+			{
+				XmlDocument xmlConfig = MyConfig();
+				XmlNode myNode = xmlConfig.SelectSingleNode("/Configuracao/DefaultValues/" + CampoDefault);
+
+				if (myNode != null)
+				{
+					myNode.InnerText = NewValorDefault;
+					xmlConfig.Save("Config.xml");
+					return true;
+				}
+				else
+				{
+					throw new Exception("O XMLNode não foi encontrado...");
+				}
+
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+		}
+
+		// SAVE CONFIG NODE VALOR CONFIGXML PELO NOME
+		// =============================================================================
+		public static bool SaveConfigValorNode(string NodeName, string NodeValue)
+		{
+			try
+			{
+				XmlDocument xmlConfig = MyConfig();
+
+				if (xmlConfig == null)
+				{
+					CriarConfigXML();
+					xmlConfig = MyConfig();
+				}
+
+				XmlNodeList elemList = xmlConfig.GetElementsByTagName(NodeName);
+
+				if (elemList.Count > 0)
+				{
+					elemList[0].InnerXml = NodeValue;
+					xmlConfig.Save("Config.xml");
+				}
+				else
+				{
+					throw new Exception("O xmlNode não foi encontrado...");
+				}
+
+				return true;
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+		}
+
+		#endregion
+
+	}
+}
--- AzureConnect/frmPrincipal.cs
+++ AzureConnect/frmPrincipal.cs
@@ -1,195 +1,306 @@
 ﻿using System;
 using System.Configuration;
+using System.Data;
 using System.Diagnostics;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Windows.Forms;
+using static AzureConnect.XMLFunctions;
 
 namespace AzureConnect
 {
-    public partial class frmPrincipal : Form
-    {
-        private string myIP;
-        private string strArgs = "";
-        private string myCommand = "az sql server firewall-rule create";
-        private string myGroup = "-g ProgramaLoja";
-        private string myServer = "-s novasiaoserver";
-        private string myRule = "";
-        private string startIP = "--start-ip-address";
-        private string endIP = "--end-ip-address";
-        private bool notOpen = false;
+	public partial class frmPrincipal : Form
+	{
+		private string myIP;
+		private string strArgs = "";
+		private string myCommand = "az sql server firewall-rule create";
+		private string myGroup = "-g GrupoIgreja";
+		private string myServer = "-s adrjserver";
+		private string myRule = "";
+		private string startIP = "--start-ip-address";
+		private string endIP = "--end-ip-address";
+		private bool notOpen = false;
 
-        // NEW FORM
-        public frmPrincipal()
-        {
+		// NEW FORM
+		public frmPrincipal()
+		{
 
-            if (!VerifyAzure.checkInstalled("CLI", "Azure"))
-            {
-                notOpen = true;
-                MessageBox.Show("Azure CLI não está instalado...");
-            }
+			if (!VerifyAzure.checkInstalled("CLI", "Azure"))
+			{
+				notOpen = true;
+				MessageBox.Show("Azure CLI não está instalado...");
+			}
 
-            InitializeComponent();
-            myIP = GetComputer_InternetIP();
-            lblIP.Text = myIP;
-            lblCommand.Text = myCommand + "...";
-            Preencher_cmbFilial();
-        }
+			InitializeComponent();
 
-        // FORM SHOW
-        private void frmPrincipal_Shown(object sender, EventArgs e)
-        {
-            if (notOpen) Close();
-        }
+			//--- try get config values: SERVER NAME AND GRUPO NAME
+			GetConfigValues();
 
-        // ON CLOSE
-        private void btnClose_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
+			//--- get IP
+			myIP = GetComputer_InternetIP();
+			lblIP.Text = myIP;
+			lblCommand.Text = myCommand + "...";
+			Preencher_cmbFilial();
 
-        // GET GLOBAL IP ADRESS
-        private string GetComputer_InternetIP()
-        {
-            // check IP using DynDNS's service
-            WebRequest request = WebRequest.Create("http://checkip.dyndns.org");
-            request.Proxy = null;
-            WebResponse response = request.GetResponse();
-            StreamReader stream = new StreamReader(response.GetResponseStream());
+			//--- create HANDLERS
+			txtGrupo.MouseDoubleClick += txt_MouseDoubleClick;
+			txtGrupo.LostFocus += txt_LostFocus;
+			txtServer.MouseDoubleClick += txt_MouseDoubleClick;
+			txtServer.LostFocus += txt_LostFocus;
+		}
 
-            // IMPORTANT: set Proxy to null, to drastically INCREASE the speed of request
+		// FORM SHOW
+		private void frmPrincipal_Shown(object sender, EventArgs e)
+		{
+			if (notOpen) Close();
+		}
 
-            // read complete response
-            string ipAddress = stream.ReadToEnd();
+		// GET XML CONFIG VALUES
+		private bool GetConfigValues()
+		{
+			try
+			{
+				//--- define config values
+				if (!VerificaConfigXML())
+				{
+					CriarConfigXML();
+				}
 
-            // close the stream
-            stream.Close();
+				//--- verifica o GRUPO
+				string tmpGrupo = ObterConfigValorNode("Grupo");
 
-            // replace everything and keep only IP
-            ipAddress = ipAddress.
-                        Replace("<html><head><title>Current IP Check</title></head><body>Current IP Address: ", string.Empty).
-                        Replace("</body></html>", string.Empty).
-                        Replace("\r\n", string.Empty);
+				if (string.IsNullOrEmpty(tmpGrupo))
+				{
+					txtGrupo.ReadOnly = false;
+					txtGrupo.Focus();
+					MessageBox.Show("Há Necessidade de Configuração do nome do GRUPO...");
+					return false;
+				}
+				else
+				{
+					txtGrupo.Text = tmpGrupo;
+					myGroup = "-g " + tmpGrupo;
+				}
 
-            return ipAddress;
-        }
+				//--- verifica o SERVER
+				string tmpServer = ObterConfigValorNode("Server");
 
-        // BTN LIBERAR PORTA AZURE
-        private void btnLiberar_Click(object sender, EventArgs e)
-        {
+				if (string.IsNullOrEmpty(tmpServer))
+				{
+					txtServer.ReadOnly = false;
+					txtServer.Focus();
+					MessageBox.Show("Há Necessidade de Configuração do nome do SERVIDOR...");
+					return false;
+				}
+				else
+				{
+					txtServer.Text = tmpServer;
+					myServer = "-s " + tmpServer;
+				}
 
-            Get_StringArgs();
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
 
-            // verify cmbFilial value
-            if (cmbFilial.SelectedItem == null)
-            {
-                MessageBox.Show("Escolha uma filial...", "Filial", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                cmbFilial.Focus();
-                return;
-            }
+		// ON CLOSE
+		private void btnClose_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
 
-            // save IP numer in list
-            try
-            {
-               SaveIPNumber();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Uma exceção ocorreu ao salvar o IPNumber no arquivo de IPs:" + 
-                    Environment.NewLine +
-                    ex.Message,
-                    "Azure Connect", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
+		// GET GLOBAL IP ADRESS
+		private string GetComputer_InternetIP()
+		{
+			// check IP using DynDNS's service
+			WebRequest request = WebRequest.Create("http://checkip.dyndns.org");
+			request.Proxy = null;
+			WebResponse response = request.GetResponse();
+			StreamReader stream = new StreamReader(response.GetResponseStream());
 
-            // execute command
-            try
-            {
-                //Process.Start("CMD.exe", "/C " + strArgs);
-                runCmdAzure();
+			// IMPORTANT: set Proxy to null, to drastically INCREASE the speed of request
 
-                // user message
-                MessageBox.Show("Liberação de Porta efetuada com sucesso!",
-                    "Azure Connect",
-                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+			// read complete response
+			string ipAddress = stream.ReadToEnd();
 
-                //quit
-                Application.Exit();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Uma exceção ocorreu ao liberar o IP no Azure DB:" +
-                Environment.NewLine +
-                ex.Message,
-                "Azure Connect", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
-        }
+			// close the stream
+			stream.Close();
 
-        private void runCmdAzure()
-        {
-            String command = @"/C " + strArgs;
-            ProcessStartInfo cmdsi = new ProcessStartInfo("cmd.exe");
-            cmdsi.Arguments = command;
-            Process cmd = Process.Start(cmdsi);
-            cmd.WaitForExit();
-        }
+			// replace everything and keep only IP
+			ipAddress = ipAddress.
+						Replace("<html><head><title>Current IP Check</title></head><body>Current IP Address: ", string.Empty).
+						Replace("</body></html>", string.Empty).
+						Replace("\r\n", string.Empty);
 
-        // FILL STRING ARGS
-        private void Get_StringArgs()
-        {
-            // define command string
-            strArgs = $"{myCommand} {myGroup} {myServer} {myRule} {startIP} {myIP} {endIP} {myIP}";
-            lblCommand.Text = strArgs;
-        }
+			return ipAddress;
+		}
 
-        // PREENCHER COMBO
-        private void Preencher_cmbFilial()
-        {
-            //fill the combobox
-            cmbFilial.Items.Add("Nova Sião");
-            cmbFilial.Items.Add("Vinde");
-            cmbFilial.Items.Add("Home");
-            cmbFilial.SelectedItem = null;
-        }
+		// BTN LIBERAR PORTA AZURE
+		private void btnLiberar_Click(object sender, EventArgs e)
+		{
 
-        // CHANGE INDEX COMBO
-        private void cmbFilial_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            // verify SelectedItem value
-            if (cmbFilial.SelectedItem == null) return;
-            // get selected Item
-            string str = (string)cmbFilial.SelectedItem;
-            //change the rule
-            myRule = ConfigurationManager.AppSettings[str];
-            //fill StringArgs
-            Get_StringArgs();
-        }
+			Get_StringArgs();
 
-        private void SaveIPNumber()
-        {
-            // define o arquivo
-            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"/AzureConnect/ConfigIPs.txt";
-            string readText = "";
-            FileInfo file = new FileInfo(path);
+			// verify cmbFilial value
+			if (cmbFilial.SelectedItem == null)
+			{
+				MessageBox.Show("Escolha uma filial...", "Filial", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				cmbFilial.Focus();
+				return;
+			}
 
-            // Open the file to read from.
-            if (file.Exists)
-            {
-                // verifica se o IP atual ja foi incluido na ultima linha 
-                string t = File.ReadLines(path).Last();
-                if (t == lblIP.Text) { return; }
+			// save IP numer in list
+			try
+			{
+				SaveIPNumber();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Uma exceção ocorreu ao salvar o IPNumber no arquivo de IPs:" +
+					Environment.NewLine +
+					ex.Message,
+					"Azure Connect", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
 
-                readText = File.ReadAllText(path);
-            }
+			// execute command
+			try
+			{
+				//Process.Start("CMD.exe", "/C " + strArgs);
+				runCmdAzure();
 
-            // Create a file to write to.
-            string createdText = readText + lblIP.Text + Environment.NewLine;
-            file.Directory.Create(); // If the directory already exists, this method does nothing.
-            File.WriteAllText(file.FullName, createdText);
+				// user message
+				MessageBox.Show("Liberação de Porta efetuada com sucesso!",
+					"Azure Connect",
+					MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-            //message box show
-            //MessageBox.Show("Arquivo de registro de IPs salvo com sucesso...", "Azure Connect",
-            //                MessageBoxButtons.OK, MessageBoxIcon.Information);
-        }
-    }
+				//quit
+				Application.Exit();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Uma exceção ocorreu ao liberar o IP no Azure DB:" +
+				Environment.NewLine +
+				ex.Message,
+				"Azure Connect", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			}
+		}
+
+		private void runCmdAzure()
+		{
+			String command = @"/C " + strArgs;
+			ProcessStartInfo cmdsi = new ProcessStartInfo("cmd.exe");
+			cmdsi.Arguments = command;
+			Process cmd = Process.Start(cmdsi);
+			cmd.WaitForExit();
+		}
+
+		// FILL STRING ARGS
+		private void Get_StringArgs()
+		{
+			// define command string
+			strArgs = $"{myCommand} {myGroup} {myServer} {myRule} {startIP} {myIP} {endIP} {myIP}";
+			lblCommand.Text = strArgs;
+		}
+
+		// PREENCHER COMBO
+		private void Preencher_cmbFilial()
+		{
+			//--- Create DataTable
+			DataTable dt = new DataTable();
+			dt.Columns.Add("Chave");
+			dt.Columns.Add("Valor");
+
+			var dic = ObterContas();
+
+			foreach (var item in dic)
+			{
+				dt.Rows.Add(new object[] { item.Key, item.Value });
+			}
+
+			//--- Set DataTable
+			cmbFilial.DataSource = dt;
+			cmbFilial.ValueMember = "Chave";
+			cmbFilial.DisplayMember = "Valor";
+			cmbFilial.SelectedItem = null;
+		}
+
+		// CHANGE INDEX COMBO
+		private void cmbFilial_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			// verify SelectedItem value
+			if (cmbFilial.SelectedItem == null) return;
+
+			// get selected Item
+			string str = (string)((DataRowView)cmbFilial.SelectedItem)["Chave"];
+
+			//change the rule
+			myRule = str;
+
+			//fill StringArgs
+			Get_StringArgs();
+		}
+
+		private void SaveIPNumber()
+		{
+			// define o arquivo
+			string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"/AzureConnect/ConfigIPs.txt";
+			string readText = "";
+			FileInfo file = new FileInfo(path);
+
+			// Open the file to read from.
+			if (file.Exists)
+			{
+				// verifica se o IP atual ja foi incluido na ultima linha 
+				string t = File.ReadLines(path).Last();
+				if (t == lblIP.Text) { return; }
+
+				readText = File.ReadAllText(path);
+			}
+
+			// Create a file to write to.
+			string createdText = readText + lblIP.Text + Environment.NewLine;
+			file.Directory.Create(); // If the directory already exists, this method does nothing.
+			File.WriteAllText(file.FullName, createdText);
+
+			//message box show
+			//MessageBox.Show("Arquivo de registro de IPs salvo com sucesso...", "Azure Connect",
+			//                MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+
+		private void lblCommand_Click(object sender, EventArgs e)
+		{
+			Clipboard.SetText(lblCommand.Text);
+			MessageBox.Show("Comando copiado para a Área de Transferência...", "Comando Copiado");
+		}
+
+
+		private void txt_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			TextBox text = (TextBox)sender;
+			text.ReadOnly = false;
+		}
+
+		private void txt_LostFocus(object sender, EventArgs e)
+		{
+			TextBox text = (TextBox)sender;
+			text.ReadOnly = true;
+
+			if (text.Name == "txtGrupo")
+			{
+				SaveDefault("Grupo", txtGrupo.Text);
+			}
+			else if (text.Name == "txtServer")
+			{
+				SaveDefault("Server", txtServer.Text);
+			}
+
+			GetConfigValues();
+		}
+
+	}
 }
